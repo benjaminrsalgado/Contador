5b10f29da2cfaee8c33caa2b864cb1073fca8508
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-06-11 13:28:19 -0600

    Initial Commit

---
+++ Contador/ContadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContadorApp.swift
+//  Contador
+//
+//  Created by Benjamin Rojo on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ContadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
