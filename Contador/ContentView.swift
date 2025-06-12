8b31ebbeab7dc9f0446ba0f7a545b45ea1452ee9
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-06-12 14:02:57 -0600

    i practice heart.fill using @StateObject and @Published

--- Contador/ContentView.swift
+++ Contador/ContentView.swift
@@ -9,42 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ContadorView()
+        ViewModel()
     }
     
 }
-
-class contador: ObservableObject{
-    @Published var contador1 = 0
-    @Published var limiteAlcanzado = false
-    func limite(){
-        if contador1 == 10{
-            limiteAlcanzado = true
-        }else{
-            limiteAlcanzado = false
-        }
+class LikeViewModel: ObservableObject{
+    @Published var leGusta = false
+    
+    func toggleLike(){
+        leGusta.toggle()
     }
 }
 
-struct ContadorView: View{
-    @StateObject var contadorModel = contador()
+struct ViewModel: View{
+    @StateObject var viewModel = LikeViewModel()
     var body: some View{
         VStack{
             Button(action:{
-                contadorModel.contador1 += 1
-                contadorModel.limite()
-            }){
-                Text("Se sumara + 1: \(contadorModel.contador1)")
-                
-            }
-            .disabled(contadorModel.limiteAlcanzado)
-            if contadorModel.limiteAlcanzado {
-                Text("Límite alcanzado")
-                    .foregroundColor(.red)
+                viewModel.toggleLike()
+            }){Label(
+                viewModel.leGusta ? "Quitar like" : "Dar like",
+                systemImage: viewModel.leGusta ? "heart.fill" : "heart"
+            )
+               
             }
         }
     }
 }
+
 
 #Preview {
     ContentView()
