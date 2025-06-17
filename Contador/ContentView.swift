967581ac00c9afe599ef87066428b03bfd3d8a5a
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-06-17 12:18:37 -0600

    i add style

--- Contador/ContentView.swift
+++ Contador/ContentView.swift
@@ -9,28 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-   ToDoView()
+        ToDoView()
     }
 }
 
-class ToDo: ObservableObject{
+
+class ToDo: ObservableObject {
     @Published var tarea: Bool = true
     
-    func check(){
+    func check() {
         tarea.toggle()
     }
 }
 
-struct ToDoView: View{
+struct ToDoView: View {
     @StateObject var tarea1 = ToDo()
-    var body: some View{
-        Button(action:{
-            tarea1.check()
-        }){Text(tarea1.tarea ? "Aprender " : "Practicar @State")
+    
+    var body: some View {
+        ZStack {
+ 
+            LinearGradient(
+                gradient: Gradient(colors: [Color.blue.opacity(0.3), Color.purple.opacity(0.3)]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
             
+            VStack(spacing: 30) {
+                Text("Mi ToDo")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.purple)
+                
+                Button(action: {
+                    withAnimation(.spring()) {
+                        tarea1.check()
+                    }
+                }) {
+                    Text(tarea1.tarea ? "Aprender" : "Practicar @State")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .padding()
+                        .frame(width: 220)
+                        .background(tarea1.tarea ? Color.green.opacity(0.8) : Color.orange.opacity(0.8))
+                        .foregroundColor(.white)
+                        .cornerRadius(16)
+                        .shadow(color: .gray.opacity(0.4), radius: 8, x: 0, y: 4)
+                        .scaleEffect(tarea1.tarea ? 1.0 : 1.1)
+                }
+            }
         }
     }
 }
+
 #Preview {
     ContentView()
 }
